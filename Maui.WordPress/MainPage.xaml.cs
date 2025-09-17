332ba7e514d5285ae33c67a1fbbc4a7bcf38b775
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2025-09-17 10:39:10 -0400

    Adding MAUI application

---
+++ Maui.WordPress/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Maui.WordPress.ViewModels;
+
+namespace Maui.WordPress
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void AddClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ Maui.WordPress/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Library.WordPress.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.WordPress.ViewModels
+{
+    public class MainViewModel
+    {
+        public List<Blog> Blogs
+        {
+            get
+            {
+                return new List<Blog>
+                {
+                    new Blog { Id = 1, Title = "Blog 1", Content = "Something great" }
+                    ,
+                    new Blog { Id = 2, Title = "Blog 2", Content = "Something great" }
+                    ,
+                    new Blog { Id = 3, Title = "Blog 3", Content = "Something great" }
+                    ,
+                    new Blog { Id = 4, Title = "Blog 4", Content = "Something great" }
+                };
+            }
+        }
+
+        public Blog? SelectedBlog { get; set; }
+    }
+}
